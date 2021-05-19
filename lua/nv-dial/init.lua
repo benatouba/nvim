7b90da5f32bee4538f3e41c09e9e226698cb6a49
Tom <grassmann@tu-berlin.de>
2021-05-19 20:05:41 +0200

    add augends to switch more/different bools

--- lua/nv-dial/init.lua
+++ lua/nv-dial/init.lua
@@ -5,6 +5,19 @@ dial.augends["custom#boolean"] = dial.common.enum_cyclic{
     strlist = {"true", "false"},
 }
 table.insert(dial.config.searchlist.normal, "custom#boolean")
+
+dial.augends["custom#Boolean"] = dial.common.enum_cyclic{
+    name = "boolean",
+    strlist = {"True", "False"},
+}
+table.insert(dial.config.searchlist.normal, "custom#Boolean")
+
+dial.augends["custom#fortran#boolean"] = dial.common.enum_cyclic{
+    name = "fortran-style boolean",
+    strlist = {"T", "F"},
+}
+table.insert(dial.config.searchlist.normal, "custom#fortran#boolean")
+
 vim.cmd([[
 nmap <C-c> <Plug>(dial-increment)
 nmap <C-x> <Plug>(dial-decrement)
