b47f85400f74a51c808978274d90761babe97859
Benjamin Schmidt <benschmidt@live.de>
2021-07-14 13:02:14 +0200

    salt-lsp still not working

--- lua/lsp/salt-ls.lua
+++ lua/lsp/salt-ls.lua
@@ -1,6 +1,30 @@
-local python_path = vim.fn.stdpath('home') .. "/.venv/nvim/bin/python3"
-local lsp_path = DATA_PATH .. "/lspinstall/salt/lsp_server.py"
-require'lspconfig'.salt.setup{
-	cmcmd = {python_path .. " " .. lsp_path , "--tcp"},
-    on_attach = require'lsp'.common_on_attach,
+local folder = vim.fn.stdpath('data') .. "/lspinstall/salt/src/salt-lsp"
+local python_path = folder .. "/.venv/bin/python3"
+local lsp_path = folder .. "/salt_lsp/__main__.py"
+local util = require "lspconfig/util"
+
+local configs = require "lspconfig/configs"
+configs["salt"] = {
+    default_config = {
+            cmd = {python_path .. " " .. lsp_path},
+            -- cmd = {python_path .. " -m salt_lsp --tcp"},
+            -- on_attach = common_on_attach,
+            filetypes = {"sls", "salt", "saltfile"},
+            root_dir = util.root_pattern(".git", "salt", vim.fn.getcwd())
+        },
+    docs = {
+        package_json = "https://raw.githubusercontent.com/redhat-developer/vscode-yaml/master/package.json",
+        description = [[
+            https://github.com/redhat-developer/yaml-language-server
+            `yaml-language-server` can be installed via `npm`:
+            ```sh
+            npm install -g yaml-language-server
+            ```
+        ]],
+        default_config = {
+            root_dir = [[root_pattern(".git", "salt", vim.fn.getcwd())]],
+        },
+    },
 }
+
+require'lspconfig'.salt.setup{}
