a07d02b477addeab42cf8807edd4eba98a8c7a7b
Benjamin Schmidt <benschmidt@live.de>
2021-12-06 10:36:14 +0100

    remove comments

--- init.lua
+++ init.lua
@@ -11,32 +11,3 @@ require('autocommands')
 if Exists(O.packer_compile_path) then
   require('packer_compiled')
 end
--- if O.language_parsing then
---   require('language_parsing')
--- end
---
--- if O.lsp then
---   require('lsp')
--- end
--- require('plugins')
--- require('nv-globals')
--- require('nv-utils')
--- require('nv-autocommands')
--- require('settings')
--- require('keymappings')
--- require('nv-galaxyline')
--- require('nv-comment')
--- require('nv-colorizer')
--- require('nv-barbar')
--- require('nv-telescope')
--- require('nv-treesitter')
--- require('nv-autopairs')
--- require('nv-which-key')
--- require('nv-dial')
--- require('nv-rooter')
--- require('nv-neogit')
---
--- vim.cmd('source ~/.config/nvim/vimscript/functions.vim')
--- require('nv-floaterm')
--- require('nv-nvim-tree')
---
