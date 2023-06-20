6b391098c9bc95b87ed07b669177468023bfde36
Benjamin Schmidt <benschmidt@live.de>
2023-06-20 12:01:20 +0200

    chore: adapt to new gitsigns config

--- lua/git/gitsigns/init.lua
+++ lua/git/gitsigns/init.lua
@@ -6,23 +6,31 @@ end
 
 gitsigns.setup({
 	signs = {
-		-- TODO add hl to colorscheme
-		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		add          = { text = '│' },
+		change       = { text = '│' },
+		delete       = { text = '契' },
+		topdelete    = { text = '契' },
+		changedelete = { text = '┆' },
+		untracked    = { text = '┆' },
 	},
+	signcolumn = true,
 	numhl = true,
-	linehl = true,
-	keymaps = {
-		buffer = true,
-	},
+	linehl = false,
+	word_diff  = false,
 	watch_gitdir = {
 		interval = 1000,
+		follow_files = true,
 	},
-	sign_priority = 11,
-	update_debounce = 200,
+	attach_to_untracked = true,
+	current_line_blame = false,
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = 'eol',
+		delay = 1000,
+	},
+	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+	sign_priority = 6,
+	update_debounce = 100,
 	status_formatter = nil, -- Use default
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
