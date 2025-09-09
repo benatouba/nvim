35f290ecf30985913048e0956adbdb1d46c51581
Benjamin Schmidt <benschmidt@live.de>
2025-09-09 21:39:16 +0200

    fix(ibl): minor setup fix

--- lua/ben/indent-blankline/init.lua
+++ lua/ben/indent-blankline/init.lua
@@ -1,27 +1,21 @@
 local M = {}
 
-M.config = function()
-	local ibl_ok, ibl = pcall(require, "ibl")
-	if not ibl_ok then
-		vim.notify("ibl not okay")
-		return
-	end
-	local highlight = {
-		-- "CursorColumn",
-		"Whitespace",
-	}
-	ibl.setup({
-		indent = { highlight = highlight, tab_char = "" },
-		scope = { enabled = true },
-		whitespace = {
-			highlight = highlight,
-			remove_blankline_trail = true,
-		},
-		exclude = {
-			buftypes = { "terminal", "fugitive", "neogit" },
-			filetypes = { "help", "startify", "dashboard", "packer", "neogitstatus", "fugitive" },
-		}
-	})
-end
+local highlight = {
+  -- "CursorColumn",
+  "Whitespace",
+}
+
+M.opts = {
+  indent = { highlight = highlight, tab_char = "" },
+  scope = { enabled = false },
+  whitespace = {
+    highlight = highlight,
+    remove_blankline_trail = true,
+  },
+  exclude = {
+    buftypes = { "terminal", "fugitive", "neogit" },
+    filetypes = { "help", "startify", "dashboard", "packer", "neogitstatus", "fugitive" },
+  },
+}
 
 return M
