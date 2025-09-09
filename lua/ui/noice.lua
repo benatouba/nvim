805621b3c35559787e4f62b83780db14a67a8846
Benjamin Schmidt <benschmidt@live.de>
2025-09-09 21:47:30 +0200

    feat(noice): c-f c-b for scrolling in popups

--- lua/ui/noice.lua
+++ lua/ui/noice.lua
@@ -7,11 +7,14 @@ M.config = function()
     return
   end
   noice.setup({
+    notify = {
+      enabled = false,
+    },
     lsp = {
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+        ["cmp.entry.get_documentation"] = false,
       },
       hover = {
         enabled = true,
@@ -48,15 +51,27 @@ M.config = function()
         },
         opts = { skip = true },
       },
-       {
+      {
         filter = {
           event = "notify",
-          min_height = 15
+          min_height = 15,
         },
-        view = 'split'
+        view = "split",
       },
     },
   })
+
+  vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+    if not require("noice.lsp").scroll(4) then
+      return "<c-f>"
+    end
+  end, { silent = true, expr = true })
+
+  vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+    if not require("noice.lsp").scroll(-4) then
+      return "<c-b>"
+    end
+  end, { silent = true, expr = true })
 end
 
 return M
