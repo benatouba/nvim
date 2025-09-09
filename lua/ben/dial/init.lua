1be9f87d3b7da1a63cfc2edd607dceaefd1ef565
Benjamin Schmidt <benschmidt@live.de>
2025-09-09 21:38:35 +0200

    fix(dial): move to lua rewrite with some new augends

--- lua/ben/dial/init.lua
+++ lua/ben/dial/init.lua
@@ -12,32 +12,48 @@ M.config = function()
 
   local augend = require("dial.augend")
 
+  local default_augends = {
+    augend.integer.alias.decimal,
+    augend.constant.alias.bool,
+    augend.constant.alias.de_weekday,
+    augend.constant.alias.de_weekday_full,
+    augend.date.alias["%d/%m/%Y"],
+    augend.constant.new({ elements = { "yes", "no" } }),
+    augend.constant.new({ elements = { "let", "const", "var" } }),
+    augend.constant.new({ elements = { "T", "F" } }),
+    augend.constant.new({ elements = { "True", "False" } }),
+    augend.constant.new({ elements = { "TRUE", "FALSE" } }),
+    augend.constant.new({ elements = { "def", "class" } }),
+    augend.hexcolor.new({ case = "lower" }),
+    augend.semver.alias.semver,
+    augend.constant.new({ elements = { "[ ]", "[x]" }, word = false, cyclic = true }),
+  }
   config.augends:register_group({
-    default = {
-      augend.integer.alias.decimal,
-      augend.constant.alias.bool,
-      augend.constant.alias.de_weekday,
-      augend.constant.alias.de_weekday_full,
-      augend.date.alias["%m/%d"],
-      augend.date.alias["%Y/%m/%d"],
-      augend.constant.new({ elements = { "yes", "no" } }),
-      augend.constant.new({ elements = { "let", "const", "var" } }),
-      augend.constant.new({ elements = { "T", "F" } }),
-      augend.constant.new({ elements = { "True", "False" } }),
-      augend.constant.new({ elements = { "TRUE", "FALSE" } }),
-      augend.constant.new({ elements = { "def", "class" } }),
-      augend.hexcolor.new({ case = "lower" }),
-      -- augend.constant.new({ elements = { "and", "or" }, word = true, cyclic = true }),
-      -- augend.constant.new({ elements = { "&&", "||" }, word = false, cyclic = true }),
-      -- augend.constant,
-      augend.semver.alias.semver,
-      augend.constant.new({ elements = { "[ ]", "[x]" }, word = false, cyclic = true }),
-    },
+    default = default_augends,
     visual = {
       augend.integer.alias.decimal,
       augend.integer.alias.hex,
       augend.date.alias["%Y/%m/%d"],
+      augend.date.alias["%d/%m/%Y"],
     },
+  })
+  require("dial.config").augends:on_filetype({
+    default = vim.tbl_extend("keep", {
+      augend.integer.alias.decimal,
+      augend.integer.alias.hex,
+      augend.constant.new({ elements = { "true", "false" } }),
+      augend.constant.new({ elements = { "let", "const" } }),
+    }, default_augends),
+    lua = vim.tbl_extend("keep", {
+      augend.integer.alias.decimal,
+      augend.constant.new({ elements = { "true", "false" } }),
+    }, default_augends),
+    markdown = vim.tbl_extend("keep", {
+      augend.integer.alias.decimal,
+      augend.misc.alias.markdown_header,
+      augend.constant.alias.de_weekday,
+      augend.constant.alias.de_weekday_full,
+    }, default_augends),
   })
 end
 
