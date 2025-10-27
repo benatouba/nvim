b2015c677135a1aaf8ae40a395106b9410953921
Benjamin Schmidt <benschmidt@live.de>
2025-10-27 16:31:31 +0100

    fix(snacks-nvim): 🐛 minor setup bugs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -148,85 +148,86 @@ lazy.setup({
   "folke/which-key.nvim",
   {
     "folke/snacks.nvim",
+    enabled = true,
     priority = 1000,
     lazy = false,
     ---@type snacks.Config
-    config = function()
-      require("snacks").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        animate = { enabled = false },
-        bigfile = { enabled = false },
-        keys = {
-          {
-            "<leader>sc",
-            function()
-              Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
-            end,
-            desc = "Find Config File",
-          },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      animate = { enabled = false },
+      bigfile = { enabled = false },
+      keys = {
+        {
+          "<leader>sc",
+          function()
+            Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+          end,
+          desc = "Find Config File",
         },
-        dashboard = {
-          enabled = true,
-          preset = {
-            keys = {
-              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-              { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-              { icon = " ", key = "t", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-              {
-                icon = " ",
-                key = "o",
-                desc = "Orgmode",
-                action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.expand('~') .. '/documents/vivere/org'})",
-              },
-              {
-                icon = " ",
-                key = "c",
-                desc = "Config",
-                action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-              },
-              { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-              { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-            },
-          },
-          sections = {
-            { section = "keys", gap = 1, padding = 1 },
-            { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-            { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+      },
+      dashboard = {
+        enabled = true,
+        preset = {
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "t", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             {
-              pane = 2,
-              icon = " ",
-              title = "Git Status",
-              section = "terminal",
-              enabled = function()
-                return Snacks.git.get_root() ~= nil
-              end,
-              cmd = "git status --short --branch --renames",
-              height = 5,
-              padding = 1,
-              ttl = 5 * 60,
-              indent = 3,
+              icon = " ",
+              key = "o",
+              desc = "Orgmode",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.expand('~') .. '/documents/vivere/org'})",
             },
-            { section = "startup" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
         },
-        rename = { enabled = false },
-        indent = { enabled = false },
-        input = { enabled = false },
-        notifier = {
-          enabled = true,
+        sections = {
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
         },
-        notify = { enabled = true },
-        quickfile = { enabled = false },
-        scroll = { enabled = false },
-        statuscolumn = { enabled = false },
-        terminal = { enabled = false },
-        toggle = { enabled = false },
-        win = { enabled = false },
-        words = { enabled = true },
-        zen = { enabled = true },
-      })
+      },
+      rename = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = false },
+      notifier = {
+        enabled = true,
+      },
+      notify = { enabled = true },
+      quickfile = { enabled = false },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      terminal = { enabled = false },
+      toggle = { enabled = false },
+      win = { enabled = false },
+      words = { enabled = true },
+      zen = { enabled = true },
+    },
+    init = function()
       vim.api.nvim_create_autocmd("LspProgress", {
         ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
         callback = function(ev)
@@ -241,31 +242,30 @@ lazy.setup({
           })
         end,
       })
-      local wk_ok, wk = pcall(require, "which-key")
-      if wk_ok then
-        wk.add({
-          {
-            "<leader>sn",
-            "<cmd>lua require('snacks').notifier.show_history()<cr>",
-            desc = "Notifications",
-            remap = false,
-          },
-          {
-            "<leader>z",
-            "<cmd>lua require('snacks').zen.zen()<cr>",
-            icon = { icon = " ", color = "yellow" },
-            desc = "Zen",
-            remap = false,
-          },
-          -- {
-          --   "<leader>sp",
-          --   "<cmd>lua Snacks.picker.projects()<cr>",
-          --   icon = " ",
-          --   desc = "Projects",
-          -- },
-        }, { mode = "n" })
-      end
     end,
+    keys = {
+      {
+        "<leader>sn",
+        function()
+          Snacks.picker.notifications()
+        end,
+        desc = "Notifications",
+      },
+      {
+        "<leader>z",
+        function()
+          Snacks.zen()
+        end,
+        desc = "Zen Mode",
+      },
+      {
+        "<leader>.",
+        function()
+          Snacks.scratch()
+        end,
+        desc = "Toggle Scratch Buffer",
+      },
+    },
   },
 
   -- Colorize hex and other colors in code,
