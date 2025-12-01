f4c7254ad65d391f6da7a31140640aec3a296ce4
Benjamin Schmidt <benschmidt@live.de>
2025-12-01 18:37:07 +0100

    feat(avante): ✨ set claude as default AI source

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1703,10 +1703,19 @@ lazy.setup({
     opts = {
       -- add any opts here
       -- for example
-      provider = "copilot",
+      provider = "claude",
       providers = {
         gemini = {
           model = "gemini-2.5-pro-exp-03-25",
+        },
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-sonnet-4-20250514",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
         },
         copilot = {
           endpoint = "https://api.githubcopilot.com",
