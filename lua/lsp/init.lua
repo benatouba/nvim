69f41210a5c8df2011cdb7576a87527589356197
Benjamin Schmidt <benschmidt@live.de>
2026-01-26 13:49:30 +0100

    fix(lsp): 🐛 fix capabilities for some LSPs

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -91,15 +91,27 @@ M.config = function()
       --   client.server_capabilities.documentFormattingProvider = true
       --   client.server_capabilities.documentRangeFormattingProvider = true
       elseif client.name == "ty" then
+        client.server_capabilities.callHierarchyProvider = false
+        client.server_capabilities.codeActionProvider = true
+        client.server_capabilities.codeLensProvider = false
+        client.server_capabilities.colorProvider = false
         client.server_capabilities.declarationProvider = true
         client.server_capabilities.definitionProvider = true
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentHighlightProvider = true
+        client.server_capabilities.documentLinkProvider = false
+        client.server_capabilities.documentOnTypeFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
+        client.server_capabilities.documentSymbolProvider = true
+        client.server_capabilities.foldingRangeProvider = false
         client.server_capabilities.hoverProvider = true
+        client.server_capabilities.implementationProvider = false
         client.server_capabilities.referencesProvider = true
         client.server_capabilities.renameProvider = true
-        client.server_capabilities.semanticTokensProvider = false
+        -- client.server_capabilities.semanticTokensProvider = false
+        client.server_capabilities.typeDefinitionProvider = true
+        client.server_capabilities.typeHierarchyProvider = false
+        client.server_capabilities.workspaceSymbolProvider = true
       elseif client.name == "mutt_ls" then
         vim.diagnostic.enable(not vim.diagnostic.is_enabled())
       elseif client.name == "ruff" then
